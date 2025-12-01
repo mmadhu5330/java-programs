71a1f9639fb536a0d5fe131e8410e809942ae4a0
mmadhu5330 <mmadhu5330@gmail.com>
2025-12-01 10:22:14 +0530

    Add files via upload

---
+++ leap year.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%4==0 || n%400==0){
+		    System.out.print("leap year");
+		}
+		else{
+		    System.out.print("Not a leap year");
+		}
+	}
+}
