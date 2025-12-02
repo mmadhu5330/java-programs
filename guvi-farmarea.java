675f4881db865b08c7f450414454d237bcf230a6
Madhumitha <mmadhu5330@gmail.com>
2025-12-02 09:35:22 +0530

    Add files via upload

---
+++ guvi-farmarea.java
@@ -0,0 +1,14 @@
+//Farm Area Calculation
+//Problem Statement:
+//Given length L and breadth B of a farm, print the area of the farm upto 5 decimal decimals.
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float L=sc.nextFloat();
+        float B=sc.nextFloat();
+        float area=L*B;
+        System.out.printf("%.5f",area);
+    }
+}
