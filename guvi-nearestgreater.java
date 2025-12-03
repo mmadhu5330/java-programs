94f2c42d67fd94a6f8da66ac997b44db7aaaf123
Madhumitha <mmadhu5330@gmail.com>
2025-12-03 21:24:56 +0530

    Add files via upload

---
+++ guvi-nearestgreater.java
@@ -0,0 +1,13 @@
+//Nearest Greater Multiple of 10
+//Problem Statement  
+//Given a number N, find the nearest greater multiple of 10.
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int greater=((N/10)+1)*10;
+        System.out.println(greater);
+    }
+}
