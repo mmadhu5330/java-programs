b96193626faa37777b6a6e89d2ca4d836c631bec
Madhumitha <mmadhu5330@gmail.com>
2025-12-03 21:02:27 +0530

    Add files via upload

---
+++ guvi-modulomulti.java
@@ -0,0 +1,15 @@
+//Modulo Multiplication
+//Problem Statement:
+//Given 3 numbers a,b,c print a*b mod c.
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=sc.nextInt();
+       int result=a*b%c;
+       System.out.println(result);
+    }
+}
