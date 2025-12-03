bc6693261971d88d68227d2f7a617e76103714c5
Madhumitha <mmadhu5330@gmail.com>
2025-12-03 21:20:11 +0530

    Add files via upload

---
+++ guvi-countingdigits.java
@@ -0,0 +1,13 @@
+//Counting Digits of a Number
+//Problem Statement 
+//Count the number of digits of a given number N.Size of the integer ranges from 1<N<100000000
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();  
+        int count = String.valueOf(N).length();
+        System.out.println(count);
+    }
+}
