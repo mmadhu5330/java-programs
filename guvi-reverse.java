59dcca5f39dd2a354094d7f27c08e3159e8ead97
Madhumitha <mmadhu5330@gmail.com>
2025-12-03 20:55:14 +0530

    Add files via upload

---
+++ guvi-reverse.java
@@ -0,0 +1,19 @@
+//Reverse a Number
+//Problem Statement:
+//Given a number N, print its reverse.
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + (n % 10);
+            n /= 10;
+        }
+
+        System.out.println(rev);
+    }
+}
